a6f09a40d1c4d72ae55e21fbc7ac7e1096ab33dc
Chen <chen2196@purdue.edu>
2017-10-16 20:02:39 -0400

    first commit

---
+++ OpenCVjustforstudy2/OpenCVjustforstudy2.cpp
@@ -0,0 +1,14 @@
+// OpenCVjustforstudy2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include"opencv2\opencv.hpp"
+#include "iostream"
+#include "string"
+
+using namespace cv;
+int main(int argv, char** argc)
+{
+	Camshi
+}
+
